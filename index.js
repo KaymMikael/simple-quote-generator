42cf5fa30d695dea624c75ccead88a730b932f47
Sero <kmcaltiz@gmail.com>
2024-06-12 12:06:12 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,387 @@
+const quotes = {
+    quote1: {
+      quote: "Human strength lies in the ability to change yourself.",
+      author: "Saitama",
+      animeTitle: "One Punch Man",
+    },
+    quote2: {
+      quote: "You can die anytime, but living takes true courage.",
+      author: "Kenshin Himura",
+      animeTitle: "Rurouni Kenshin",
+    },
+    quote3: {
+      quote:
+        "Fear is not evil. It tells you what weakness is. And once you know your weakness, you can become stronger as well as kinder.",
+      author: "Gildarts Clive",
+      animeTitle: "Fairy Tail",
+    },
+    quote4: {
+      quote: "If you don’t share someone’s pain, you can never understand them.",
+      author: "Nagato",
+      animeTitle: "Naruto",
+    },
+    quote5: {
+      quote:
+        "You will never be able to love anybody else until you love yourself.",
+      author: "Lelouch Lamperouge",
+      animeTitle: "Code Geass",
+    },
+    quote6: {
+      quote:
+        "All we can do is live until the day we die. Control what we can…and fly free.",
+      author: "Deneil Young",
+      animeTitle: "Space Brothers",
+    },
+    quote7: {
+      quote: "If you don't take risks, you can't create a future.",
+      author: "Monkey D. Luffy",
+      animeTitle: "One Piece",
+    },
+    quote8: {
+      quote:
+        "There’s no shame in falling down! True shame is to not stand up again!",
+      author: "Shintaro Midorima",
+      animeTitle: "Kuroko’s Basketball",
+    },
+    quote9: {
+      quote:
+        "When you hit the point of no return, that’s the moment it truly becomes a journey. If you can still turn back, it’s not really a journey.",
+      author: "Hinata Miyake",
+      animeTitle: "A Place Further than the Universe",
+    },
+    quote10: {
+      quote:
+        "The world isn’t perfect. But it’s there for us, doing the best it can… that’s what makes it so damn beautiful.",
+      author: "Roy Mustang",
+      animeTitle: "Full Metal Alchemist",
+    },
+    quote11: {
+      quote: "Simplicity is the easiest path to true beauty.",
+      author: "Seishuu Handa",
+      animeTitle: "Barakamon",
+    },
+    quote12: {
+      quote:
+        "The past is the past. We cannot indulge ourselves in memories and destroy the present.",
+      author: "Murata Ken",
+      animeTitle: "Kyou Kara Maou!",
+    },
+    quote13: {
+      quote:
+        "Knowing what it feels like to be in pain, is exactly why we try to be kind to others.",
+      author: "Jiraiya",
+      animeTitle: "Naruto",
+    },
+    quote14: {
+      quote:
+        "Mistakes are not shackles that halt one from stepping forward. Rather, they are that which sustain and grow one's heart.",
+      author: "Mavis Vermillon",
+      animeTitle: "Fairy Tail",
+    },
+    quote15: {
+      quote: "We need to stop living for others. From now on…Let’s live for ourselves!",
+      author: "Historia Reiss",
+      animeTitle: "Attack on Titan",
+    },
+    quote16: {
+      quote: "Helping other people is the best way to make up for your mistakes.",
+      author: "Kenshin Himura",
+      animeTitle: "Rurouni Kenshin",
+    },
+    quote17: {
+      quote: "Everything has a beginning and an end. Life is just a cycle of starts and stops. There are ends we don’t desire, but they’re inevitable, we have to face them. It’s what being human is all about.",
+      author: "Jet Black",
+      animeTitle: "Cowboy Bebop",
+    },
+    quote18: {
+      quote: "Whether a fish lives in a clear stream or a water ditch, so long as it continues swimming forward, it will grow up beautifully.",
+      author: "Koro-sensei",
+      animeTitle: "Assassination Classroom",
+    },
+    quote19: {
+      quote: "Like I always say, can’t find a door? Make your own.",
+      author: "Edward Elric",
+      animeTitle: "Fullmetal Alchemist",
+    },
+    quote20: {
+      quote: "People who continue to put their lives on the line to defend their faith become heroes and continue to exist on in legend.",
+      author: "Naruto Uzumaki",
+      animeTitle: "Naruto",
+    },
+    quote21: {
+      quote: "Believe in your own power.",
+      author: "Mikasa Ackerman",
+      animeTitle: "Attack on Titan",
+    },
+    quote22: {
+      quote: "As the living it is our responsibility to carry out the wishes of the ones who are gone.",
+      author: "Akame",
+      animeTitle: "Akame Ga Kill",
+    },
+    quote23: {
+      quote: "Those who stand at the top determine what’s wrong and what’s right! This very place is neutral ground! Justice will prevail, you say? But, of course, it will! Whoever wins this war becomes justice!",
+      author: "Donquixote Doflamingo",
+      animeTitle: "One Piece",
+    },
+    quote24: {
+      quote: "Forgetting is like a wound. The wound may heal, but it has already left a scar.",
+      author: "Monkey D. Luffy",
+      animeTitle: "One Piece",
+    },
+    quote25: {
+      quote: "You need to accept the fact that you’re not the best and have all the will to strive to be better than anyone you face.",
+      author: "Roronoa Zoro",
+      animeTitle: "One Piece",
+    },
+    quote26: {
+      quote: "People who can’t throw something important away can never hope to change anything.",
+      author: "Armin Arlert",
+      animeTitle: "Attack on Titan",
+    },
+    quote27: {
+      quote: "If you begin to regret, you’ll dull your future decisions and let others make your choices for you. All that’s left for you then is to die. Nobody can foretell the outcome. Each decision you make holds meaning only by affecting your next decision.",
+      author: "Erwin Smith",
+      animeTitle: "Attack on Titan",
+    },
+    quote28: {
+      quote: "Fools who don’t respect the past are likely to repeat it.",
+      author: "Nico Robin",
+      animeTitle: "One Piece",
+    },
+    quote29: {
+      quote: "A life that lives without doing anything is the same as a slow death.",
+      author: "Lelouch",
+      animeTitle: "Code Geass: Lelouch of the Rebellion",
+    },
+    quote30: {
+      quote: "The moment you find the courage to give up your life for someone… would be the moment you understand love.",
+      author: "Kenshin Himura",
+      animeTitle: "Samurai X",
+    },
+    quote31: {
+      quote: "When you fall in love with someone, you start wanting to know lots of things about that person.",
+      author: "Ninako Kinoshita",
+      animeTitle: "Strobe Edge",
+    },
+    quote32: {
+      quote: "Even if I lose this feeling, I’m sure I’ll just fall in love with you all over again.",
+      author: "Syaoran Li",
+      animeTitle: "Cardcaptor Sakura",
+    },
+    quote33: {
+      quote: "Even if I searched the world over, no one could compare to you.",
+      author: "Hikaru Hitachiin",
+      animeTitle: "Ouran Highschool Host Club",
+    },
+    quote34: {
+      quote: "Love is simply an electrical bug in the human neural circuit.",
+      author: "Akasaka Ryuunosuke",
+      animeTitle: "My Pet Girl Sakurasou",
+    },
+    quote35: {
+      quote: "However little strength I’m capable of, I’ll do everything humanly possible to protect the people I love.",
+      author: "Roy Mustang",
+      animeTitle: "Fullmetal Alchemist",
+    },
+    quote36: {
+      quote: "Love is like a mirror that reflects your bad side. Especially when it’s unrequited, you get envious, jealous, prejudiced, and resentful. You have to face all sorts of emotions, but there’s no reason to find that shameful.",
+      author: "Margery Daw",
+      animeTitle: "Shakugan No Shana",
+    },
+    quote37: {
+      quote: "Once you fall in love, you really love that person through thick and thin.",
+      author: "Oka Chinami",
+      animeTitle: "Golden Time!",
+    },
+    quote38: {
+      quote: "If it’s possible for one person to be hurt by another, then it’s possible for that person to be healed by another.",
+      author: "Sohma Hatori",
+      animeTitle: "Fruits Basket",
+    },
+    quote39: {
+      quote: "Even If I lose this feeling, I’m sure that I’ll just fall in love with you all over again.",
+      author: "Syaoran Li",
+      animeTitle: "Cardcaptor Sakura",
+    },
+    quote40: {
+      quote: "When it comes to people you really love, you don’t care about yourself.",
+      author: "Kurosawa Yamato",
+      animeTitle: "Say I Love You",
+    },
+    quote41: {
+      quote: "When a man learns to love, he must bear the risk of hatred.",
+      author: "Madara Uchiha",
+      animeTitle: "Naruto",
+    },
+    quote42: {
+      quote: "I love you with all my heart!.. If you were to stay here with me, there would be no regrets…because every day we’d do something fun, we’d be happy, I swear..! I would do anything for you! So…please, just stay with me!",
+      author: "Sakura Haruno",
+      animeTitle: "Naruto",
+    },
+    quote43: {
+      quote: "You changed me! Your smile is what saved me! That is why I’m not afraid to die protecting you! Because I love you.",
+      author: "Hyuuga Hinata",
+      animeTitle: "Naruto",
+    },
+    quote44: {
+      quote: "Love is never as simple as sharing the same path.",
+      author: "Khamsin",
+      animeTitle: "Shakugan No Shana",
+    },
+    quote45: {
+      quote: "People cannot win against their loneliness.",
+      author: "Gaara",
+      animeTitle: "Naruto",
+    },
+    quote46: {
+      quote: "I’ve gained freedom…and yet… it’s so lonesome...",
+      author: "Sohma Momiji",
+      animeTitle: "Fruits Basket",
+    },
+    quote47: {
+      quote: "Living is anxiety and pain. It's continuing to think, continuing to choose.",
+      author: "Alcor",
+      animeTitle: "Devil Survivor",
+    },
+    quote48: {
+      quote: "Humans die. Animals die. Plants die. Even soul reapers die. It’s the arch of the universe. Everything that comes to life eventually ceases to exist.",
+      author: "Baraggan Louisenbairn",
+      animeTitle: "Bleach",
+    },
+    quote49: {
+      quote: "The scariest and the most painful thing is to be hated by someone you truly love.",
+      author: "Tohru Honda",
+      animeTitle: "Fruits Basket",
+    },
+    quote50: {
+      quote: "When do you think people die? When they are shot through the heart by the bullet of a pistol? No. When they are ravaged by an incurable disease? No. When they drink a soup made from a poisonous mushroom? No! It’s when… they are forgotten.",
+      author: "Dr. Hiruluk",
+      animeTitle: "One Piece",
+    },
+    quote51: {
+      quote: "To know sorrow is not terrifying. What is terrifying is to know you can’t go back to the happiness you could have.",
+      author: "Matsumoto Rangiku",
+      animeTitle: "Bleach",
+    },
+    quote52: {
+      quote: "Remember that everyone you meet is afraid of something, loves something, and has lost something.",
+      author: "Lucy Heartfilia",
+      animeTitle: "Fairy Tail",
+    },
+    quote53: {
+      quote: "Never trust anyone too much, remember, the devil was once an angel.",
+      author: "Ken Kaneki",
+      animeTitle: "Tokyo Ghoul",
+    },
+    quote54: {
+      quote: "I always, always wondered, I was born to die, so what was my reason for existing in this world?",
+      author: "Yuuki Konno",
+      animeTitle: "Sword Art Online",
+    },
+    quote55: {
+      quote: "Whatever you lose, you’ll find it again. But what you throw away, you’ll never get back.",
+      author: "Kenshin Himura",
+      animeTitle: "Rurouni Kenshin",
+    },
+    quote56: {
+      quote: "Death is simply another stage of our life.",
+      author: "Grand Elder Guru",
+      animeTitle: "Dragon Ball Z",
+    },
+    quote57: {
+      quote: "Loneliness is a sickness that leads to death.",
+      author: "Holo",
+      animeTitle: "Spice and Wolf",
+    },
+    quote58: {
+      quote: "All this time, I seriously thought that it’s better to die than to live your life alone.",
+      author: "Kirito",
+      animeTitle: "Sword Art Online",
+    },
+    quote59: {
+      quote: "It’s just pathetic to give up on something before you even give it a shot.",
+      author: "Reiko Mikami",
+      animeTitle: "Another",
+    },
+    quote60: {
+      quote: "The only way to truly escape the mundane is for you to constantly be evolving. Whether you choose to aim high, or aim low. Enjoy each day for what it is.",
+      author: "Izaya Orihara",
+      animeTitle: "Durarara!!",
+    },
+    quote61: {
+      quote: "The ticket to the future is always open.",
+      author: "Vash the Stampede",
+      animeTitle: "Trigun",
+    },
+    quote62: {
+      quote: "No matter how deep the night, it always turns to day, eventually.",
+      author: "Brook",
+      animeTitle: "One Piece",
+    },
+    quote63: {
+      quote: "Miracles don’t happen by just sitting around. We have to make them happen ourselves.",
+      author: "Kiyoshi Fujino",
+      animeTitle: "Prison School",
+    },
+    quote64: {
+      quote: "The moment you think of giving up, think of the reason why you held on so long.",
+      author: "Natsu Dragneel",
+      animeTitle: "Fairy Tail",
+    },
+    quote65: {
+      quote: "Destiny. Fate. Dreams. These unstoppable ideas are held deep in the heart of man. As long as there are people who seek freedom in this life, these things shall not vanish from the Earth.",
+      author: "Gol D. Roger",
+      animeTitle: "One Piece",
+    },
+    quote66: {
+      quote: "Life is not a game of luck. If you wanna win, work hard.",
+      author: "Sora",
+      animeTitle: "No Game No Life",
+    },
+    quote67: {
+      quote: "Every journey begins with a single step. We just have to have patience.",
+      author: "Milly Thompson",
+      animeTitle: "Trigun",
+    },
+    quote68: {
+      quote: "Learn to treasure your life because unfortunately, it can be taken away from you anytime.",
+      author: "L Lawliet",
+      animeTitle: "Death Note",
+    },
+    quote69: {
+      quote: "There’s no such thing as a painless lesson. They just don’t exist. Sacrifices are necessary. You can’t gain anything without losing something first.",
+      author: "Edward Elric",
+      animeTitle: "Fullmetal Alchemist",
+    },
+    quote70: {
+      quote: "Sometimes I do feel like I’m a failure. Like there’s no hope for me. But even so, I’m not gonna give up. Ever!",
+      author: "Deky aka Izuku Midoriyo",
+      animeTitle: "My Hero Academia",
+    },
+    quote71: {
+      quote: "To know sorrow is not terrifying. What is terrifying is to know you can’t go back to happiness you could have.",
+      author: "Matsumoto Rangiku",
+      animeTitle: "Bleach",
+    },
+    quote72: {
+      quote: "You should enjoy the little detours to the fullest. Because that’s where you’ll find the things more important than what you want.",
+      author: "Ging Freecss",
+      animeTitle: "Hunter x Hunter",
+    },
+    quote73: {
+      quote: "People’s lives don’t end when they die, it ends when they lose faith.",
+      author: "Itachi Uchiha",
+      animeTitle: "Naruto",
+    },
+  };
+  
+
+function generateQuote() {
+  const randomNum = Math.ceil(Math.random() * 73);
+  const quote = quotes[`quote${randomNum}`].quote;
+  const author = quotes[`quote${randomNum}`].author;
+  const title = quotes[`quote${randomNum}`].animeTitle;
+  document.querySelector(".anime-quote").innerHTML = `"${quote}" - ${author}, <span class="quote-anime-title">${title}</span>`;
+}
+
+generateQuote();
